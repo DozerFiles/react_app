7a35aeb3ba8e8654f4101398134612f688f6c306
hayden <haydencardoza@outlook.com>
2021-03-03 13:22:46 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from 'axios'
+import Loading from './Loading'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    //create state
+    this.state = {
+      users:[],
+      loading: false
+    }
+    //binding handleSubmit
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  getUsers(){ 
+    this.setState({
+      loading: true
+    })
+
+    axios('https://randomuser.me/api/?results=5').then(response => 
+      this.setState({
+        users: [...this.state.users, ...response.data.results],
+        loading: false
+      })
+    );
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    this.getUsers();
+    console.log('more users loaded')
+  }
+
+  UNSAFE_componentWillMount(){
+    this.getUsers();
+  }
+
+  render() {
+    return <div className="App">
+      {!this.state.loading ? ( 
+        this.state.users.map(users => (
+          <div>
+            <h3>{users.name.first}</h3>
+            <p>{users.email}</p>
+            <hr/>
+            <form onSubmit={this.handleSubmit}>
+              <input type="submit" value="load users"/>
+            </form>
+          </div>
+        ))
+      ) : (<Loading message="This is my loading message"/>) }
+    </div>;
+  }
 }
 
 export default App;
